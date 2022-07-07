86b9c83e7e28ced6ce71ac2c47e69b755add56ec
Administrator <Administrator@10.0.0.41>
2022-07-07 02:00:22 -0400

    working stage commented

--- Automation1/src/ASSIGNMENTS/Assignment6.java
+++ Automation1/src/ASSIGNMENTS/Assignment6.java
@@ -17,12 +17,20 @@ public class Assignment6 {
 
 		driver.get("https://jqueryui.com/droppable/");
 		driver.manage().window().maximize();
+		// 1st Step we have to create an object for Actions 
 		Actions action = new Actions(driver);
+		
+		//2nd get the frame element and and switch the driver to frame becuse inside the frame we have to do the drag and drop
 		WebElement fram = driver.findElement(By.xpath("//*[@id=\"content\"]/iframe"));
 		driver.switchTo().frame(fram);
+		
+		// 3rd get the dragable element
 		WebElement drag = driver.findElement(By.id("draggable"));
+		//4th get the dropable element
 		WebElement drop = driver.findElement(By.id("droppable"));
+		
 		Thread.sleep(2000);
+		// 5th Use the action class (dragAndDrop method) and put the perform
 		action.dragAndDrop(drag, drop).perform();
 		// action.clickAndHold(drag).moveToElement(drop).release().build().perform();
 		// action.dragAndDrop(drag, drop).perform();
